6d87b7c0845dfdcbac351ca5b9b1fc3a5f178004
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-27 15:45:10 -0600

    finished lab

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -3,12 +3,19 @@ const bcrypt = require("bcryptjs");
 
 module.exports = {
   login: (req, res) => {
-    console.log("Logging In User");
-    console.log(req.body);
     const { username, password } = req.body;
+
     for (let i = 0; i < users.length; i++) {
-      if (users[i].username === username && users[i].password === password) {
-        res.status(200).send(users[i]);
+      if (users[i].username === username) {
+        const authenticated = bcrypt.compareSync(
+          password,
+          users[i].passwordHash
+        );
+        if (authenticated) {
+          let userToReturn = { ...users[i] };
+          delete userToReturn.passwordHash;
+          res.status(200).send(userToReturn);
+        }
       }
     }
     res.status(400).send("User not found.");

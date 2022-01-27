d808c1a927b9bcdaa8ee6b8e53808a7fc2f92483
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-27 15:18:21 -0600

    initial commit

---
+++ part-1/server/controllers/auth.js
@@ -0,0 +1,32 @@
+const users = [];
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  login: (req, res) => {
+    console.log("Logging In User");
+    console.log(req.body);
+    const { username, password } = req.body;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username === username && users[i].password === password) {
+        res.status(200).send(users[i]);
+      }
+    }
+    res.status(400).send("User not found.");
+  },
+  register: (req, res) => {
+    const { username, email, firstName, lastName, password } = req.body;
+    const salt = bcrypt.genSaltSync(5);
+    const passwordHash = bcrypt.hashSync(password, salt);
+    let user = {
+      username,
+      email,
+      firstName,
+      lastName,
+      passwordHash,
+    };
+    users.push(user);
+    let userToReturn = { ...user };
+    delete userToReturn.passwordHash;
+    res.status(200).send(userToReturn);
+  },
+};
